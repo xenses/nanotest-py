59dbeb21845702187310c8418212d5d17de374e1
Shawn Boyette <shawn@firepear.net>
2011-08-20 15:59:57 -0400

    silent better

--- tests/00-pis_pisnt.py
+++ tests/00-pis_pisnt.py
@@ -9,11 +9,14 @@ pis(nanotest._is_core(1,1), True, "identity");
 pis(nanotest._is_core(1,0), False, "nonidentity");
 nanotest.nanotest_run -= 2; # make the two inner tests go away for counting purposes
 
-nanotest.nanotest_silent = True
 pis(nanotest.nanotest_pass, 5, "5 tests passing before pis fail")
+nanotest.nanotest_silent = True
 pis(1, 0, "pis() failure: Of course zero doesn't equal one.");
+nanotest.nanotest_silent = False
 pis(nanotest.nanotest_pass, 6, "6 tests passing after pis fail")
+nanotest.nanotest_silent = True
 pisnt(1, 1, "pisnt() failure: Of course one equals one.");
+nanotest.nanotest_silent = False
 pis(nanotest.nanotest_pass, 7, "7 tests passing after pisnt fail")
 nanotest.nanotest_pass += 2; # smooth over failing tests, which we will treat as passing:)
 
