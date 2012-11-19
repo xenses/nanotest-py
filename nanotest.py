b9c89a346a0766ea6002d461d4afcd07f8c5cc45
Shawn Boyette <shawn@firepear.net>
2012-11-19 12:19:19 -0500

    first things first: tear out all the reporting code and docstrings

--- nanotest.py
+++ nanotest.py
@@ -1,24 +1,3 @@
-"""This module implements a tiny slice of the functionality of Perl's
-Test::More. Its job is to make unit/whitebox testing quick and
-easy.
-
-It has a counterpart script, also called 'nanotest', which acts as the
-test harness/runner. The harness searches for test scripts in and
-below the current directory, runs them, reports their individual
-findings, then issues a summary for the entire test suite.
-
-Test scripts must live in a directory named 'tests', and they must
-have the extension '.py'. Scripts are simply Python programs and may
-be written however you please.
-
-This module is used by doing
-
-    from nanotest import *
-
-in each test script. Four functions will be exported into your
-namespace: three for testing and one for reporting. They are detailed
-below."""
-
 import re
 
 nanoconf = { 'run':0,
@@ -42,23 +21,7 @@ def _is_core(expr, given):
             return True
     return False
 
-def pis(expr, given, msg):
-    """Test for equivalence.
-
-pis() should be named is(), but 'is' is a keyword in Python.
-
-Takes 3 arguments: an experimental value, a given value, and a
-message. The first two can be any valid Python expression; the third
-should be a message string.
-
-If the experimental and given values are equivalent, the test is a
-success and nothing happens. If they are different, the test is a
-failure and the message will be printed to STDOUT.
-
-The given value may be a string in the form of ':re:PATTERN', which
-will cause a regex evaluation of the experimental value against
-PATTERN instead of a bare equivalance check.
-"""
+def _is(expr, given, msg):
     global nanoconf
     passed = _is_core(expr, given)
     if  passed:
@@ -70,13 +33,7 @@ PATTERN instead of a bare equivalance check.
             _print_is_fail_msg(expr=expr, given=given, msg=msg)
     return passed
 
-def pisnt(expr, given, msg):
-    """Test for difference.
-
-pisnt() is named so that it will match pis().
-
-Works exactly like pis(), but backwards: tests succeed if the
-experimental and the given values are NOT equivalent."""
+def _isnt(expr, given, msg):
     global nanoconf
     passed = not _is_core(expr, given)
     if  passed:
@@ -90,26 +47,7 @@ experimental and the given values are NOT equivalent."""
 
 #-----------------------------------------------------------------------
 
-def pis_deeply(expr, given, msg):
-    """Test composite datastructures for equivalence.
-
-Like pis()and pisnt(), pis_deeply() takes three arguments: an
-experimental value, a given value, and a message. The first two should
-be composite data structures (lists, dictionaries, tuples), and they
-may be as deeply nested as you please. The third should be a message
-string.
-
-The experimental and given structures will be tested for "congruence"
-(that is, they have the same structure and the same values at all
-endpoints of the structure). If they do not match, the test fails and
-the message will be printed to STDOUT.
-
-Since this comparison is more complex than the one performed by
-pis/nt(), additional information about the exact nature of the failure
-will also be printed.
-
-Any value in the given struct may be a regex string as described in
-the pydoc for pis()."""
+def _is_deeply(expr, given, msg):
     # reset state
     global nanoconf
     nanoconf['error'] = False
@@ -191,66 +129,3 @@ def _regex_comp(key=None, **kw):
     else:
         _set_err(reason="renomatch", errkey=key)
         return False
-
-#-----------------------------------------------------------------------
-
-# _set_err - housekeeping
-# keywords: reason (code), errkey (deephash key or None)
-def _set_err(**kw):
-    global nanoconf
-    nanoconf['error'] = True
-    nanoconf['errcode'] = kw['reason']
-    nanoconf['errkey'] = kw['errkey']
-
-
-# _print_is_fail_msg - pis/pisnt nonregex failure
-# keywords: expr, given, msg, pisnt
-def _print_is_fail_msg(pisnt=False, **kw):
-    if nanoconf['silent']:
-        return
-    print("FAILED test {}: {}".format(nanoconf['run'], kw['msg']))
-    if pisnt:
-        print("   Expected anything but '{}' and got it anyway".format(kw['given']))
-    else:
-        print("   Expected: '{}'".format(kw['given']))
-        print("   Got     : '{}'".format(kw['expr']))
-
-
-# _print_is_fail_msg - pis/pisnt regex failure
-# keywords: expr, given, msg, pisnt
-def _print_re_fail_msg(pisnt=False, **kw):
-    if nanoconf['silent']:
-        return
-    print("FAILED test {}: {}".format(nanoconf['run'], kw['msg']))
-    if pisnt:
-        print("   Didn't expect '{}' to match regex '{}', but it does".format(kw['expr'], kw['given']))
-    else:
-        print("   '{}' does not match regex '{}'".format(kw['expr'], kw['given']))
-
-
-def _print_deep_fail_msg(msg, expr, given):
-    if nanoconf['silent']:
-        return
-    print("FAILED test {}: {}".format(nanoconf['run'], msg))
-    if nanoconf['errcode'] == "badvalue":
-        print("   Values at {} don't match".format(nanoconf['errkey']))
-        print("   Expected '{}'; got '{}'".format(expr, given))
-    elif nanoconf['errcode'] == "nomatchinexpr":
-        print("   Node {} exists in given struct but not experimental struct".format(nanoconf['errkey']))
-    elif nanoconf['errcode'] == "nomatchingiven":
-        print("   Node {} exists in experimental struct but not given struct".format(nanoconf['errkey']))
-
-
-def _print_deep_re_fail_msg(msg, expr, given):
-    if nanoconf['silent']:
-        return
-    print("FAILED test {}: {}".format(nanoconf['run'], msg))
-    print("   Node {} ({}) does not match regex '{}'".format(nanoconf['errkey'], expr, given))
-
-
-def nanotest_summary():
-    """Utility function which prints the number of tests run and
-    passed. Should be called at the end of every test script."""
-    print("{} {}".format(nanoconf['run'], nanoconf['pass']))
-
-__all__ = ["pis", "pisnt", "pis_deeply", "nanotest_summary"]
