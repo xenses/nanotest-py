e99b674df63696cb3a8a297e632c7ca8f4ae2be8
Shawn Boyette <shawn@firepear.net>
2012-11-22 21:52:57 -0500

    more tests

--- tests/00-basic.py
+++ tests/00-basic.py
@@ -8,10 +8,25 @@ n.untest(0, 1, "basic inverted identity test")
 n.test(isinstance(n, nanotest.Nanotester), True, "nanotest object type test")
 n.test(len(n.results), 3, "three successful tests up to now")
 
-n.test(n.results[0]["file"], "./tests/00-basic.py", "tests in this file")
-n.test(n.results[0]["line"], 5, "first test was on line 5")
-n.test(n.results[0]["pass"], True, "first test passed")
-n.test(n.results[0]["xpect"], None, "don't store expected values for successful tests")
-n.test(n.results[0]["got"], None, "don't store received values for successful tests")
-n.test(n.results[0]["msg"], None, "don't store msg for successful tests")
-n.test(n.results[0]["reason"], None, "don't store reason for successful tests")
+test1 = n.results[0]
+n.test(test1["file"], "./tests/00-basic.py", "tests in this file")
+n.test(test1["line"], 5, "first test was on line 5")
+n.test(test1["pass"], True, "first test passed")
+n.test(test1["xpect"], None, "don't store expected values for successful tests")
+n.test(test1["got"], None, "don't store received values for successful tests")
+n.test(test1["msg"], None, "don't store msg for successful tests")
+n.test(test1["reason"], None, "don't store reason for _is_eq tests")
+
+n.test(0, 1, "generating a failure")
+test2 = n.results[11]
+n.test(test2["file"], "./tests/00-basic.py", "tests in this file")
+n.test(test2["line"], 20, "11th test was on line 20")
+n.test(test2["pass"], False, "11th test failed")
+n.test(test2["xpect"], 1, "expected '1'")
+n.test(test2["got"], 0, "expected '0'")
+n.test(test2["msg"], "generating a failure", "msg was 'generating a failure'")
+n.test(test2["reason"], None, "no reason for _is_eq tests")
+# elide failing test before reporting gets to it
+a = n.results[:10]
+b = n.results[12:]
+n.results = a + b
