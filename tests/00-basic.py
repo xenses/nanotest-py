0fa8c976d33f415aa689bc9987a45445ed2cfc2e
Shawn Boyette <shawn@firepear.net>
2012-11-24 01:13:15 -0500

    fixing tests for new result layout

--- tests/00-basic.py
+++ tests/00-basic.py
@@ -12,20 +12,20 @@ test1 = n.results[0]
 n.test(test1["file"], "./tests/00-basic.py", "tests in this file")
 n.test(test1["line"], 5, "first test was on line 5")
 n.test(test1["pass"], True, "first test passed")
-n.test(test1["xpect"], 1, "1")
-n.test(test1["got"], 1, "also 1")
 n.test(test1["msg"], "basic identity test", "msg")
-n.test(test1["reason"], None, "don't store reason for _is_eq tests")
+n.test(test1["comp"][0]["xpect"], 1, "1")
+n.test(test1["comp"][0]["got"], 1, "also 1")
+n.test(test1["comp"][0]["reason"], None, "don't store reason for _is_eq tests")
 
 n.test(0, 1, "generating a failure")
 test2 = n.results[11]
 n.test(test2["file"], "./tests/00-basic.py", "tests in this file")
 n.test(test2["line"], 20, "11th test was on line 20")
 n.test(test2["pass"], False, "11th test failed")
-n.test(test2["xpect"], 1, "expected '1'")
-n.test(test2["got"], 0, "expected '0'")
 n.test(test2["msg"], "generating a failure", "msg was 'generating a failure'")
-n.test(test2["reason"], None, "no reason for _is_eq tests")
+n.test(test2["comp"][0]["xpect"], 1, "expected '1'")
+n.test(test2["comp"][0]["got"], 0, "expected '0'")
+n.test(test2["comp"][0]["reason"], None, "no reason for _is_eq tests")
 # elide failing test before reporting gets to it
 a = n.results[:11]
 b = n.results[12:]
--- tests/01-re.py
+++ tests/01-re.py
@@ -14,10 +14,10 @@ test1 = n.results[6]
 n.test(test1["file"], "./tests/01-re.py", "tests in this file")
 n.test(test1["line"], 12, "7th test was on line 12")
 n.test(test1["pass"], False, "7th test failed")
-n.test(test1["got"], "foobar", "'foobar' was the string to match")
-n.test(test1["xpect"], "\s", "'\s' was the regexp to match against")
 n.test(test1["msg"], "no spaces, will fail", "msg was 'no spaces, will fail'")
-n.test(test1["reason"], "regexp failure ('got' is not a match for 'expected')",
+n.test(test1["comp"][0]["got"], "foobar", "'foobar' was the string to match")
+n.test(test1["comp"][0]["xpect"], "\s", "'\s' was the regexp to match against")
+n.test(test1["comp"][0]["reason"], "regexp failure ('got' is not a match for 'expected')",
        "regexp failures do have reasons set")
 # elide failing test before reporting gets to it
 a = n.results[:6]
