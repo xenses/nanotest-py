5cd3f8ebca5e60be1ea2293848e22ed7a9310cf9
Shawn Boyette <shawn@firepear.net>
2012-11-28 12:46:02 -0500

    oops. wrong name. checkin to fix.

--- tests/03-hash.py
+++ tests/03-hash.py
@@ -2,3 +2,11 @@ import nanotest
 
 n = nanotest.Nanotester()
 
+recv = {}
+
+t1 = ['a', 'b', 4]
+
+n._hash(t1, recv)
+
+n.test(len(recv), 3, "recv hash should be 3 elements")
+n.test(recv["l0"], 'a', "key 'l0', value 'a'")
