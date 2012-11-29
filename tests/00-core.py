941495a9f6f97ea2eb85e37e1a867b3074cbd405
Shawn Boyette <shawn@firepear.net>
2012-11-29 15:30:47 -0500

    filename change

--- tests/00-core.py
+++ tests/00-core.py
@@ -9,7 +9,7 @@ n.test(isinstance(n, nanotest.Nanotester), True, "nanotest object type test")
 n.test(len(n.results), 3, "three successful tests up to now")
 
 test1 = n.results[0]
-n.test(test1["file"], "./tests/00-basic.py", "tests in this file")
+n.test(test1["file"], "./tests/00-core.py", "tests in this file")
 n.test(test1["line"], 5, "first test was on line 5")
 n.test(test1["pass"], True, "first test passed")
 n.test(test1["msg"], "basic identity test", "msg")
@@ -19,7 +19,7 @@ n.test(test1["comp"][0]["reason"], None, "don't store reason for _is_eq tests")
 
 n.test(0, 1, "generating a failure")
 test2 = n.results[11]
-n.test(test2["file"], "./tests/00-basic.py", "tests in this file")
+n.test(test2["file"], "./tests/00-core.py", "tests in this file")
 n.test(test2["line"], 20, "11th test was on line 20")
 n.test(test2["pass"], False, "11th test failed")
 n.test(test2["msg"], "generating a failure", "msg was 'generating a failure'")
