24fc46bf1718ec1a45f154cd7eb6cb73950612f9
Shawn Boyette <shawn@firepear.net>
2013-01-09 21:08:47 -0500

    2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup( name = "nanotest",
-       version = "2.0.1",
+       version = "2.0.2",
        description = "Tiny testing toolkit (for Python)",
        author = "Shawn Boyette",
        author_email = "shawn@firepear.net",
