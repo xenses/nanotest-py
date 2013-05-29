39be6582563161b252048b45892e0f547ac312ac
Shawn Boyette <shawn@firepear.net>
2013-05-29 15:23:08 +0000

    00-core tests now passing

--- nanotest/__init__.py
+++ nanotest/__init__.py
@@ -1,7 +1,6 @@
-import inspect
 import re
 
-import nanotest.core
+import nanotest.core as nc
 
 class Nanotester:
     """Initialize a nanotest tester object."""
@@ -13,18 +12,17 @@ class Nanotester:
         self.ghash = {}     # given struct hash
         self.re_re   = re.compile("\:re\:")
         self.re_type = re.compile("\:ty\:")
-        self.nc      = nanotest.core
 
     def test(self, xpmtl, given, msg, invert=False):
         """Test two values for equality"""
         if type(xpmtl) != type(given):
-            res = self.nc.result(False, type(given), type(xpmtl), msg, "Types don't match")
+            res = nc.result(self, False, type(given), type(xpmtl), msg, "Types don't match")
             self.results.append(res)
         elif isinstance(xpmtl, (tuple, list, dict)):
             self.nc.deepcomp(xpmtl, given, msg, invert)
         else:
-            passed, reason = self.nc.comp(xpmtl, given, msg, invert)
-            self.results.append(self.nc.result(passed, given, xpmtl, msg, reason))
+            passed, reason = nc.comp(self, xpmtl, given, msg, invert)
+            self.results.append(nc.result(self, passed, given, xpmtl, msg, reason))
                 
     def untest(self, xpmtl, given, msg):
         """Test two values for inequality"""
