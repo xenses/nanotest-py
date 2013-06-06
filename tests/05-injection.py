c166498268ff8a8dce154da8193e9fe32772384e
Shawn Boyette <shawn@firepear.net>
2013-06-06 13:41:35 +0000

    all source failure modes tested

--- tests/05-injection.py
+++ tests/05-injection.py
@@ -4,6 +4,12 @@ import nanotest.injection as ni
 n = nanotest.Nanotester()  # the object we're using to report our results
 n2 = nanotest.Nanotester() # the object we're exercising code with
 
+# failure modes
 n2.inject("foo")
 n.test(n2.results[-1]["msg"], ":re:could not open", "doesn't exist")
 
+n2.inject("./tests/corpus/05-injection-notjson.txt")
+n.test(n2.results[-1]["msg"], ":re:could not unmarshal JSON", "not valid json")
+
+n2.inject("./tests/corpus/05-injection-notdict.json")
+n.test(n2.results[-1]["msg"], ":re:Injected test data should be dict", "top struct isn't a dict")
