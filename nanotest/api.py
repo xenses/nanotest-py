47b873eb7f198103364d1121871c9a688e515b93
Shawn Boyette <shawn@firepear.net>
2013-06-06 03:36:39 +0000

    first cut of source()

--- nanotest/api.py
+++ nanotest/api.py
@@ -1,2 +1,22 @@
 import json
+import nanotest.core as nc
+
+def source (obj, filename):
+    try:
+        src = open(filename, "r")
+    except Exception as err:
+        msg = "could not open: {}".format(err)
+        obj.results.append({'run': False, 'msg': msg, 'file': test, 'tests': None})
+        return
+    try:
+        injt = json.load(src)
+    except Exception as err:
+        msg = "could not unmarshal JSON: {}".format(err)
+        obj.results.append({'run': False, 'msg': msg, 'file': test, 'tests': None})
+        return
+    if type(injt) != "dict":
+        msg = "Injected test data should be dict; is {}".format(type(injt))
+        obj.results.append({'run': False, 'msg': msg, 'file': test, 'tests': None})
+        return
+    obj.injt = injt
 
